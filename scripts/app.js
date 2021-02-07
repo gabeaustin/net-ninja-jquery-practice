4bb94d863cd4120b93c3d5230a632b8e4761041f
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-06 18:09:03 -0600

    added events

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+let tweet = "<div style='margin: 20px 0; padding: 10px; background: #eee;>The big fight live: Ham vs Cheese!</div>";
+
+// $("#contact").append("That's just a test.")
+
+$("#contact").append(tweet);
+
+
+/* ADDING & CHANGING CONTENT
+
+.append() --> adds content to bottom of the element
+.prepend() --> adds content to top of element
+.before() --> adds content before element
+.after() --> adds content after element
+.html() --> changes the whole html of the element
+.text() --> changes the text of an element
+
+
+
+*/
